cb406991248099b0b0a61cbdfbab7ed8437adc41
Archit Yadav <archityadav97@gmail.com>
2016-11-14 12:19:06 +0530

    KeyWordMatrix
    This file works upon any keyword given by the user.

---
+++ KeywordMatrix.java
@@ -0,0 +1,83 @@
+package keywordmatrix;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Archit
+ */
+/*----------------------------------------------------------*/
+/*----------------------------------------------------------*/
+/*
+This class works upon a specific keyword which is entered
+by the user. It could be anything, ranging from name of
+imaginary planet to your favourite word. But it should be
+something which is not too well known to the world.
+*/
+/*----------------------------------------------------------*/
+/*----------------------------------------------------------*/
+public class KeywordMatrix 
+{
+    static int []arr = new int[26];
+    
+    static int fetchCharacterPosition(char c)
+    {
+        int no = c;
+        return (no-96);
+    }
+    
+    static int mouldTheArray(char ch)
+    {
+       int []tempArray = new int[26];
+       
+       int posToStop = fetchCharacterPosition(ch);
+       //System.out.println("----");
+       //System.out.println(posToStop);
+       //System.out.println("----");
+       for(int i = 0; i<26; i++)
+       {
+           
+           tempArray[i] = arr[i];
+           if((i+1)==posToStop)
+           {
+               for(int downCounter = posToStop, iterator = 0; downCounter>0; downCounter--, iterator++)
+               {
+                   arr[iterator] = tempArray[downCounter-1];
+               }
+               break;
+           }
+           
+       }
+       return 0; 
+    }
+    
+    static void initializeTheArray()//To be called once.
+    {
+        for(int i = 0; i<26; i++)
+        {
+            arr[i] = i+1;
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        initializeTheArray();
+        String string = new String();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your keyword: ");
+        //No blank spaces in the keyword...for now.
+        string = scanner.next();
+        //System.out.println(fetchCharacterPosition('e'));
+        for(int i = 0; i<7; i++)
+        {
+            //System.out.println(string.charAt(i));
+            mouldTheArray(string.charAt(i));
+            
+        }
+        for(int i=0; i<26; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    
+}

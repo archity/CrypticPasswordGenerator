504de0b4ad771c37801733759eb1c1043b875086
Archit Yadav <archityadav97@gmail.com>
2016-11-17 14:40:09 +0530

    Minor update
    Just cleaning up the code a little bit... :)

--- KeywordMatrix.java
+++ KeywordMatrix.java
@@ -30,12 +30,9 @@ public class KeywordMatrix
        int []tempArray = new int[26];
        
        int posToStop = fetchCharacterPosition(ch);
-        //System.out.println("----");
-        //System.out.println(posToStop);
-        //System.out.println("----");
+        
        for(int i = 0; i<26; i++)
        {
-           
            tempArray[i] = arr[i];
            if((i+1)==posToStop)
            {
@@ -44,8 +41,7 @@ public class KeywordMatrix
                    arr[iterator] = tempArray[downCounter-1];
                }
                break;
-           }
-           
+           }   
        }
        return 0; 
     }
@@ -66,12 +62,10 @@ public class KeywordMatrix
         System.out.println("Enter your keyword: ");
             //No blank spaces in the keyword...for now.
         string = scanner.next();
-            //System.out.println(fetchCharacterPosition('e'));
+            
         for(int i = 0; i<string.length(); i++)// loop will run till the word length
         {
-            //System.out.println(string.charAt(i));
-            mouldTheArray(string.charAt(i));
-            
+            mouldTheArray(string.charAt(i));   
         }
         for(int i=0; i<26; i++)
         {

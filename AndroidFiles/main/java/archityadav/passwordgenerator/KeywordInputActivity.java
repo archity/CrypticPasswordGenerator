c5abe6eee289d2460ed265530b6ace7a74d39857
Archit Yadav <archityadav97@gmail.com>
2016-11-20 23:41:22 +0530

    AndroidFiles

---
+++ AndroidFiles/main/java/archityadav/passwordgenerator/KeywordInputActivity.java
@@ -0,0 +1,13 @@
+package archityadav.passwordgenerator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KeywordInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keyword_input);
+    }
+}
---
+++ AndroidFiles/main/java/archityadav/passwordgenerator/UsernameInputActivity.java
@@ -0,0 +1,13 @@
+package archityadav.passwordgenerator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UsernameInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_username_input);
+    }
+}

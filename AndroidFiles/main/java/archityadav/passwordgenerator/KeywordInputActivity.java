6a0547711c5bc1811a0912554caf383dd9252de3
Archit Yadav <archityadav97@gmail.com>
2016-11-24 21:22:29 +0530

    Android layout change...
    Changing the layout of the activities to have the websitename and
    username in the same activity. Also added the transition effect.

--- AndroidFiles/main/java/archityadav/passwordgenerator/KeywordInputActivity.java
+++ AndroidFiles/main/java/archityadav/passwordgenerator/KeywordInputActivity.java
@@ -1,13 +1,33 @@
 package archityadav.passwordgenerator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class KeywordInputActivity extends AppCompatActivity {
+public class KeywordInputActivity extends AppCompatActivity
+{
+    EditText keywordEditText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keyword_input);
+
+        keywordEditText = (EditText) findViewById(R.id.keywordEditText);
+    }
+
+    public void gotoMainActivity(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.putExtra("keyword", keywordEditText.getText());   //key, value
+        /* Would send this data to the next activity...*/
+
+        Intent nextActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(nextActivityIntent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        /* Goes to the nextactivity UsernameInput...*/
     }
 }
--- AndroidFiles/main/java/archityadav/passwordgenerator/WebsiteAndUsernameInputActivity.java
+++ AndroidFiles/main/java/archityadav/passwordgenerator/WebsiteAndUsernameInputActivity.java
@@ -4,33 +4,41 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class UsernameInputActivity extends AppCompatActivity
+public class WebsiteAndUsernameInputActivity extends AppCompatActivity
 {
 
-    //Button button = (Button) findViewById(R.id.nextButton1);
-    EditText usernameEditText;
+    EditText websiteEditText, usernameEditText;
+    TextView usernameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_username_input);
+        setContentView(R.layout.activity_website_input);
 
+        websiteEditText = (EditText) findViewById(R.id.websiteEditText);
         usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        usernameTextView = (TextView) findViewById(R.id.usernameTextView);
 
+        websiteEditText.bringToFront();
+        usernameEditText.bringToFront();
+        usernameTextView.bringToFront();
     }
 
     public void gotoNextActivity(View view)
     {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        intent.putExtra("username", usernameEditText.getText());   //key, value
+        intent.putExtra("site", websiteEditText.getText());   //key, value
+        intent.putExtra("username", usernameEditText.getText());
         /* Would send this data to the next activity...*/
 
         Intent nextActivityIntent = new Intent(getApplicationContext(), KeywordInputActivity.class);
         startActivity(nextActivityIntent);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-        /* Goes to the nextactivity UsernameInput...*/
+        /* Goes to the next activity KeywordInput...*/
+
     }
+
 }
